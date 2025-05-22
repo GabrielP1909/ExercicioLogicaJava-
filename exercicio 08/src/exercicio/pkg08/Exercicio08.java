aa5a1f305e1994f3d0a89cd14073ab3f4000eb89
GabrielP1909 <gabrielneves681p@gmail.com>
2025-05-21 21:39:46 -0300

    Não finalizado

--- exercicio 08/src/exercicio/pkg08/Exercicio08.java
+++ exercicio 08/src/exercicio/pkg08/Exercicio08.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package exercicio.pkg08;
 
-/**
- *
- * @author neves
- */
+import java.util.Scanner;
+
 public class Exercicio08 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner objNr = new Scanner(System.in);
+
+        int[] nmrs = new int[3];
+
+        System.out.println("Digite um número inteiro");
+        nmrs[0] = objNr.nextInt();
+        System.out.println("Digite um segundo número inteiro");
+        nmrs[1] = objNr.nextInt();
+        System.out.println("Digite um terceiro número inteiro");
+        nmrs[2] = objNr.nextInt();
+
+        
+        
     }
-    
 }
